5432e3f45fd5eab6c150755cf1f7f8d5f6d61297
Priyanka-Nawale <108576618+Priyanka-Nawale@users.noreply.github.com>
2023-06-25 19:43:30 +0530

    Add project files.

---
+++ CrudOpeartion/Controllers/EmployeeController.cs
@@ -0,0 +1,86 @@
+﻿using CrudOpeartion.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CrudOpeartion.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        private readonly EmploeedataAccess employeeDataAccess;
+
+        public EmployeeController()
+        {
+            string filepath = "C:\\Users\\user\\source\\repos\\Task\\CrudOpeartion\\Data\\TextFile1.txt";
+            employeeDataAccess = new EmploeedataAccess(filepath);
+        }
+        public ActionResult Index()
+        {
+            List<Employee> employees = employeeDataAccess.GetAllEmployees();
+
+            return View(employees);
+        }
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(Employee employee)
+        {
+            employeeDataAccess.Create(employee);
+            return RedirectToAction("Index");
+        }
+        [HttpGet]
+        public ActionResult Edit(int id)
+        {
+            List<Employee> employees = employeeDataAccess.GetAllEmployees();
+            Employee existingEmployee = employees.FirstOrDefault(e => e.Id == id);
+            return View(existingEmployee);
+        }
+        [HttpPost]
+        public ActionResult Edit(Employee employee)
+        {
+            employeeDataAccess.Update(employee);
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Details(int id)
+        {
+            List<Employee> employees = employeeDataAccess.GetAllEmployees();
+            Employee existingEmployee = employees.FirstOrDefault(e => e.Id == id);
+            return View(existingEmployee);
+        }
+
+        [HttpGet]
+        [ActionName("Delete")]
+        public ActionResult Delete(int id)
+        {
+            List<Employee> employees = employeeDataAccess.GetAllEmployees();
+            Employee existingEmployee = employees.FirstOrDefault(e => e.Id == id);
+            return View(existingEmployee);
+        }
+
+        [HttpPost]
+        [ActionName("Delete")]
+        public ActionResult Delete_Post(int id)
+        {
+            employeeDataAccess.Delete(id);
+            return RedirectToAction("Index");
+
+
+        }
+
+    }
+
+
+
+
+
+
+
+
+
+    }
---
+++ CrudOpeartion/Models/EmploeedataAccess.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.IO;
+
+namespace CrudOpeartion.Models
+{
+    public class EmploeedataAccess
+    {
+        private readonly string filePath;
+
+        public EmploeedataAccess(string filePath)
+        {
+            this.filePath = filePath;
+        }
+
+        //To store data in text file
+        private void SaveEmployees(List<Employee> employees)
+        {
+            List<string> lines = employees.Select(e => $"Employee Id:{e.Id},Employee Name:{e.Name},Department:{e.Department}").ToList();
+            File.WriteAllLines(filePath, lines);
+        }
+
+
+        public List<Employee> GetAllEmployees()
+        {
+            List<Employee> employees = new List<Employee>();
+            string[] lines = File.ReadAllLines(filePath);
+
+            foreach(string line in lines)
+            {
+                string[] data = line.Split(',');
+                Employee employee = new Employee()
+                {
+                    Id =int.Parse(data[0].Split(':')[1]),
+                    Name=data[1].Split(':')[1],
+                    Department=data[2].Split(':')[1]
+                };
+                employees.Add(employee);
+            }
+            return employees;
+        }
+
+        public void Create(Employee employee)
+        {
+            List<Employee> employees = GetAllEmployees();
+
+            //To generate new id for new employee
+            int newid = employees.Count > 0 ? employees.Max(e => e.Id) + 1 : 1;
+            employee.Id = newid;
+
+            employees.Add(employee);
+            SaveEmployees(employees);
+        }
+
+        public void Update(Employee employee)
+        {
+            List<Employee> employees = GetAllEmployees();
+            Employee existingEmployee = employees.FirstOrDefault(e => e.Id == employee.Id);
+
+            if(existingEmployee!=null)
+            {
+                existingEmployee.Name = employee.Name;
+                existingEmployee.Department = employee.Department;
+                existingEmployee.DOB = employee.DOB;
+
+                SaveEmployees(employees);
+            }
+        }
+
+        public void Delete(int id)
+        {
+            List<Employee> employees = GetAllEmployees();
+            Employee employeeDelete = employees.FirstOrDefault(e => e.Id == id);
+            if(employeeDelete!=null)
+            {
+                employees.Remove(employeeDelete);
+                SaveEmployees(employees);
+
+            }
+
+  
+        }
+    }
+}
